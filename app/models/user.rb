9109b9d86cd19dadf19b70fc8a7c4c53de096ce4
Deborah Hamel <deborahleehamel@gmail.com>
2016-12-12 12:44:43 -0700

    iteration 1 complete user can sign up, sign in and sign out add model spec for user uppdate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :links
-
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   validates_confirmation_of :password
 
+  has_many :movies 
 end
